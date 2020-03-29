f18193fc257a24d4100e9fdafe32897c485df59e
Garv Maggu <17963209+GarvMaggu@users.noreply.github.com>
2020-03-29 16:56:10 +0530

    Update product_item.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
 
-final String id;
-final String title;
-final String imageUrl;
-
-ProductItem(this.id, this.title, this.imageUrl);
+  ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+            icon: Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: (){},
+        ),
+      ),
     );
   }
 }
